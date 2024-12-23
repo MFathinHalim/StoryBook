0444a1cae34477f0dec46f9964ef617fa86c4618
halimfathin7 <halimfathin7@gmail.com>
2024-12-23 09:42:52 +0000

    p

--- src/app/api/comment/[id]/route.ts
+++ src/app/api/comment/[id]/route.ts
@@ -12,7 +12,7 @@ export async function GET(req: NextRequest) {
     if(!user) return NextResponse.json({msg: "Invalid Authentication."}, { status: 401 })
 
     const id = req.nextUrl.pathname.split("/")[3]
-    const book = await bookInstance.GetBooks(id)
+    const book = await bookInstance.GetBook(id)
 
     if (!book) return NextResponse.json({ msg: "Book not found!" }, { status: 404 })
 
