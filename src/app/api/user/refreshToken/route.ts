a14982243f76a774c98951d4a5747551e5a9db94
M.Fathin Halim <halimfathin7@gmail.com>
2025-01-24 12:23:56 +0700

    test

--- src/app/api/user/refreshToken/route.ts
+++ src/app/api/user/refreshToken/route.ts
@@ -9,5 +9,6 @@ export async function POST(req:NextRequest) {
     const rt = req.cookies.get("refreshtoken")?.value
     if (!rt) return NextResponse.json({ error: 'Missing Refresh Token' }, { status: 401 })
     const token = await userInstance.createRefreshToken(rt)
+    if(token === "error") return
     return NextResponse.json({ token })
 }
